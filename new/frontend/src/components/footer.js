1191437a98a6f86b59c72ef9042b87533d552f16
纳贝流士 <63221794+DefinitelyNotJah@users.noreply.github.com>
2020-11-29 22:08:38 +0100

    Turned it into a component instead of a function

--- new/frontend/src/components/footer.js
+++ new/frontend/src/components/footer.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
 import Typography from '@material-ui/core/Typography';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = (theme) => ({
   root: {
     flexGrow: 1,
   },
@@ -14,31 +14,35 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(2),
     color: theme.palette.text.secondary,
   },
-}));
+});
 
-export default function footermyass() {
-  const classes = useStyles();
-  return (
-    <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Paper className={classes.paper} >
-            <Typography variant="caption" display="block" gutterBottom>
-              ToS
-                The information on this website is intended only for research purposes. Access has been given to security or other researchers for the purposes of research ONLY. Other than that I sincerely hope this data helps you with your projects and aids in your understanding of your or your organization's security posture. Much love.
+class footerLower extends React.Component {
+	render () {
+		const { classes }  = this.props;
+	  return (
+	    <div className={classes.root}>
+	      <Grid container spacing={3}>
+	        <Grid item xs={12}>
+	          <Paper className={classes.paper} >
+	            <Typography variant="caption" display="block" gutterBottom>
+	              ToS
+	                The information on this website is intended only for research purposes. Access has been given to security or other researchers for the purposes of research ONLY. Other than that I sincerely hope this data helps you with your projects and aids in your understanding of your or your organization's security posture. Much love.
 
-                - _hyp3ri0n
-            </Typography>
-            <Typography variant="caption" display="block" gutterBottom>
-              ToS Enforcement
-The information on this website is intended only for research purposes. We search common database leak sites to ensure this is not being circulated for malicious use. We do have default logging turned on for this website which likely includes IP addresses and other identifiers. Please don't misuse this information as we will cooperate with law enforcement if it is.
-By the way, for old users, by popular request there is no longer a cryptominer on this website or any ads.
+	                - _hyp3ri0n
+	            </Typography>
+	            <Typography variant="caption" display="block" gutterBottom>
+	              ToS Enforcement
+	The information on this website is intended only for research purposes. We search common database leak sites to ensure this is not being circulated for malicious use. We do have default logging turned on for this website which likely includes IP addresses and other identifiers. Please don't misuse this information as we will cooperate with law enforcement if it is.
+	By the way, for old users, by popular request there is no longer a cryptominer on this website or any ads.
 
-- _hyp3ri0n
-            </Typography>
-          </Paper>
-        </Grid>
-      </Grid>
-    </div>
-  );
+	- _hyp3ri0n
+	            </Typography>
+	          </Paper>
+	        </Grid>
+	      </Grid>
+	    </div>
+  		);
+	}
 }
+
+export default withStyles(useStyles)(footerLower)
